02bf00964eb31e17fe6b16d9784053a23b055648
Manu Phatak <manu@thinkcerca.com>
2017-09-24 15:53:25 -0500

    add wsgi entry point

--- src/server.py
+++ src/server.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "Hello World!"
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
