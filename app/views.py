ac3e69a79a20734b573d94fcc0cc1eec84a092b0
Manu Phatak <manu@thinkcerca.com>
2017-09-24 22:15:59 -0500

    wip find post format

--- app/views.py
+++ app/views.py
@@ -5,5 +5,7 @@ from flask import request
 @app.route("/", methods=['POST'])
 def index():
 
+    app.logger.info(request.data)
     app.logger.info(request.form)
+    app.logger.info(request.values)
     return "{'response': 'Ok'}"
