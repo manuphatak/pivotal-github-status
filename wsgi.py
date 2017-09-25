177d7e7fdf3b5401e68a41060f5e0e76e1fff628
Manu Phatak <manu@thinkcerca.com>
2017-09-24 22:01:11 -0500

    print log

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from app import app
+import logging
+import sys
 
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 if __name__ == '__main__':
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
-    app.logger.setLevel(logging.DEBUG)
     app.run()
